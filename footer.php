6350a38e42464e9bef8118ea8ae64961abe67176
Smilichenko <110824059+SergSmilya@users.noreply.github.com>
2024-03-02 17:32:18 +0200

    added footer with adaptive

--- footer.php
+++ footer.php
@@ -1,6 +1,30 @@
         <footer class="footer" >
-            <div class="container-footer">
-                <h2 class="footer-text">Fooetr  footer.php</h2>
+            <div class="container">
+               <div class="footer-main__wrap">
+                    <div class="footer__inner-wrap">
+                        <a href="" class="logo__link">THIS NEED TO ADD LOGO</a>
+
+                    <ul class="privacy__list">
+                        <li class="privacy__item">
+                            <a href="#" target="_blank" rel="noopener noreferrer" class="privacy__link">
+                            Захист даних</a>
+                        </li>
+                        <li class="privacy__item privacy__item-order">
+                            <a href="#" target="_blank" rel="noopener noreferrer" class="privacy__link">
+                            Політика конфіденційності</a>
+                        </li>
+                    </ul>
+
+                    <ul class="social-media__list">
+                        <li class="social-media__item"><a class="social-media__link" href="#" target="_blank" rel="noopener noreferrer">
+                            <svg class="social__icon"><use xlink:href="wp-content/themes/ukraida/src/images/footer/sprites.svg#instagram"></use></svg>@ukraida_darmstadt</a></li>
+                        <li class="social-media__item"><a class="social-media__link" href="mailto:" target="_blank" rel="noopener noreferrer">
+                            <svg class="social__icon"><use xlink:href="wp-content/themes/ukraida/src/images/footer/sprites.svg#email"></use></svg>email@gmail.com</a></li>
+                    </ul>
+                    </div>
+
+                    <p class="copyright__desc">&#169; 2024  БО «Ukraida». Всі права захищені.</p>
+               </div>
             </footer>
         </div
         <?php wp_footer(); ?>  
