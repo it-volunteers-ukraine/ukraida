e0a53cdf389cf25fd1cecb04c75b83f3e15b778e
NilKad <kadulin@gmail.com>
2024-03-02 22:54:21 +0100

    v0

--- parts/next-event.php
+++ parts/next-event.php
@@ -1,10 +1,4 @@
 
-<h2>Наступна Демонстрація ...  parts/next-event</h2>
-<div class="button-example">
-    <button type='button' class='button-main'>btn-main</button>
-    <button type='button' class='button-advanced'>button-advanced</button>
-    <button type='button' class='button-advanced-big'>button-advanced-big</button>
-</div>
 <div class="event next-event__event">
     <p class="event-title">
     Наступна Демонстрація за мир в Україні відбудеться 03.02.2024 р. за адресою Friedensplatz (Darmstadt) о 10:30 год.
