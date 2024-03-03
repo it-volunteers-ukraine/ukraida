6b5b2af2a66c1f5faa3de9d809adbfda076baae9
NilKad <kadulin@gmail.com>
2024-03-03 14:19:00 +0100

    next-event

--- parts/next-event.php
+++ parts/next-event.php
@@ -3,7 +3,7 @@
     <p class="event-title">
     Наступна Демонстрація за мир в Україні відбудеться 03.02.2024 р. за адресою Friedensplatz (Darmstadt) о 10:30 год.
     </p>
-    <p class="event-title">
+    <!-- <p class="event-title">
         Die nächste Demonstration für den Frieden in der Ukraine findet am 3. Februar 2024 um 10:30 Uhr auf dem Friedensplatz (Darmstadt) statt.
     </p>
     <p class="event-title-inter">
@@ -12,6 +12,6 @@
     </p>
     <p class="event-title">
         Die nächste Demonstration für den Frieden in der Ukraine findet am 3. Februar 2024 um 10:30 Uhr auf dem Friedensplatz (Darmstadt) statt.
-    </p>
+    </p> -->
     <button class="event-btn"> Показати на карті</button>
 </div>
