31f6f890d02eaeaf48c4f0bca12f0f1306823272
gladja <gladkih.alexander@gmail.com>
2024-03-04 23:16:51 +0200

    created header styled and add burger menu

--- header.php
+++ header.php
@@ -1,16 +1,78 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
     <title>Ukraida</title>
 </head>
-<body>  
-        <header class="header" >
-            <h1>Header</h1>
-        </header> 
-        
-        
-	
+
+<body>
+    <header class="header">
+        <container class="wrap">
+            <button class="modal js-open-menu">
+                <img src="wp-content/themes/ukraida/src/images/header/menu.svg" alt="menu" />
+            </button>
+            <nav class="nav">
+                <a href="#" class="logo">UKRAIDA<img class="nav__img" src="wp-content/themes/ukraida/src/images/header/logo.svg" alt="Logo" /></a>
+                <ul class="nav__site">
+                    <li class="nav__item"><a class="link" href="#">Головна</a></li>
+                    <li class="nav__item"><a class="link" href="#">Про нас</a></li>
+                    <li class="nav__item"><a class="link" href="#">Новини</a></li>
+                    <li class="nav__item"><a class="link" href="#">Пожертви</a></li>
+                    <li class="nav__item"><a class="link" href="#">Долучитися</a></li>
+                    <li class="nav__item"><a class="link" href="#">Проєкти</a></li>
+                </ul>
+            </nav>
+            <ul class="menu">
+                <li>
+                    <a href="#" class="menu__item-inst"><img src="wp-content/themes/ukraida/src/images/header/instagram.svg" alt="instagram" /></a>
+                </li>
+                <li>
+                    <a href="#" class="menu__item"><img class="menu__img" src="wp-content/themes/ukraida/src/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE</span>/UA</a>
+                </li>
+                <li><button class="menu__btn">Задонатити</button></li>
+            </ul>
+
+            <!-- mobile menu -->
+        </container>
+        <div class="menu__overlay">
+            <div class="js-menu-container">
+                <ul class="menu__modal">
+                    <li>
+                        <a href="#" class="menu__modal-item"><img class="menu__modalimg" src="wp-content/themes/ukraida/src/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE&nbsp;</span>/&nbsp;UA</a>
+                    </li>
+                    <li>
+                        <button class="modal js-close-menu">
+                            <img src="wp-content/themes/ukraida/src/images/header/close.svg" alt="close" width="17" />
+                        </button>
+                    </li>
+                </ul>
+                <ul class="nav__modal">
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Головна</a>
+                    </li>
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Про нас</a>
+                    </li>
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Новини</a>
+                    </li>
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Пожертви</a>
+                    </li>
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Долучитися</a>
+                    </li>
+                    <li class="nav__item-modal">
+                        <a class="link-modal" href="#">Проєкти</a>
+                    </li>
+                </ul>
+                <div class="menu__item-modal">
+                    <a href="#"><img class="nav__img-inst" src="wp-content/themes/ukraida/src/images/header/instagram.svg" alt="instagram" width="36" /></a>
+                </div>
+            </div>
+        </div>
+    </header>
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,1 +1,19 @@
-console.log("header");
+// console.log("header");
+const mobileMenu = document.querySelector(".js-menu-container");
+const openMenuBtn = document.querySelector(".js-open-menu");
+const overlayMenu = document.querySelector(".menu__overlay");
+const closeMenuBtn = document.querySelector(".js-close-menu");
+
+const toggleMenu = () => {
+  mobileMenu.classList.toggle("is__open");
+  overlayMenu.classList.toggle("backdrop");
+};
+
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  mobileMenu.classList.remove("is__open");
+  overlayMenu.classList.remove("backdrop");
+});
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
