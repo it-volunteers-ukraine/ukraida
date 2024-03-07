014485ea46e39d0f9e2ff12c80f2fd6a71b116e2
Smilichenko <110824059+SergSmilya@users.noreply.github.com>
2024-03-07 11:22:07 +0200

    common styled
    add icon-close

--- parts/modal-donate.php
+++ parts/modal-donate.php
@@ -2,6 +2,32 @@
 
 <div class="modal">
     <div class="container">
-        <div class="inner__wrap"></div>
+        <div class="inner__wrap">
+            <!-- button-close -->
+            <button type="button" class="btn-clear btn-icon modal__btn-close">
+                <svg class="modal__icon-btn"><use href="wp-content/themes/ukraida/src/images/footer/sprites.svg#close"></use></svg>
+            </button>
+
+            <!-- text -->
+            <div class="modal__text-wrap">
+                <h2 class="modal__title">Міжнародний благодійний фонд "UKRAIDA" </h2>
+                <ul class="payment-list">
+                    <li class="payment-item">
+                        <p class="payment-text">ЄДРПОУ: <span>00000000</span></p>
+                    </li>
+                    <li class="payment-item">
+                        <p class="payment-text payment-text--size">IBAN: <span>UA000000000000000000000000</span></p>
+                    </li>
+                    <li class="payment-item">
+                        <p class="payment-text">АТ КБ <span>“Приват Банк”</span></p>
+                    </li>
+                </ul>
+                <h3 class="modal__subtitle">Призначення платежу:</h3>
+                <p class="modal__subtitle-text">“Безповоротна фінансова допомога від <span>ПІБ</span>”</p>
+            </div>
+
+            <!-- button-copy -->
+            <button type="button" class="modal__btn-copy btn-clear">Копіювати<span class="modal__btn-copy--span"> IBAN</span></button>
+        </div>
     </div>
 </div>
