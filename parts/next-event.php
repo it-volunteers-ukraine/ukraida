656a3a2b27389628144d38899bc2240f7889f9b2
NilKad <kadulin@gmail.com>
2024-03-07 12:33:27 +0100

    v1

--- parts/next-event.php
+++ parts/next-event.php
@@ -1,8 +1,9 @@
+<?php echo do_shortcode('[instagram-feed feed=1]'); ?>
 
 <div class="event next-event__event">
     <!-- <br>font Lato: <br> -->
     <p class="event-title">
-    Наступна Демонстрація за мир в Україні відбудеться 03.02.2024 р. за адресою Friedensplatz (Darmstadt) о 10:30 год.
+        Наступна Демонстрація за мир в Україні відбудеться 03.02.2024 р. за адресою Friedensplatz (Darmstadt) о 10:30 год.
     </p>
     <!-- <p class="event-title">
         Die nächste Demonstration für den Frieden in der Ukraine findet am 3. Februar 2024 um 10:30 Uhr auf dem Friedensplatz (Darmstadt) statt.
@@ -16,4 +17,43 @@
     </p>
      <button class="event-btn"> Показати на карті</button> -->
     <a href="#event-map" class="event-btn">Показати на карті</a>
+    <div class='card'>
+        <div class='blur-wrap'>
+            <div class='blur-el-b'></div>
+            <div class='blur-el-y'></div>
+        </div>
+        <div class="card-text">
+            <p class="card-number">+8</P>
+            <p class="card-content">Успішно проведених проєктів</P>
+        </div>
+    </div>
+    <div class='card2'>
+        <div class='blur-wrap2'>
+            <div class='blur-el-b2'></div>
+            <div class='blur-el-y2'></div>
+        </div>
+        <div class="card-text">
+            <p class="card-number">+8</P>
+            <p class="card-content">Успішно проведених проєктів</P>
+        </div>
+    </div>
+    <div class='card3'>
+        <div class='blur-wrap2'>
+            <div class='blur-el-b2'></div>
+            <div class='blur-el-y2'></div>
+        </div>
+        <div class="card-text">
+            <p class="card-number">+8</P>
+            <p class="card-content">Успішно проведених проєктів</P>
+        </div>
+    </div>
+    <!-- <div class='card'>
+        <div class='blur-block'>
+            <div class="blur-block-el"></div>
+        </div>
+        <div class="card-text">
+            <p class="card-number">+8</P>
+            <p class="card-content">Успішно проведених проєктів</P>
+        </div>
+    </div> -->
 </div>
