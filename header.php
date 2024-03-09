830c4d0b534d8c546f101d6770bfbfee599c00ea
gladja <gladkih.alexander@gmail.com>
2024-03-09 15:16:32 +0200

    refactor _global.scss change img src

--- header.php
+++ header.php
@@ -13,9 +13,9 @@
     <header class="header">
         <div class="wrap container">
             <button class="modal js-open-menu">
-                <img class="btn__img" src="wp-content/themes/ukraida/src/images/header/menu.svg" alt="menu" />
+                <img class="btn__img" src="<?php bloginfo('template_url'); ?>/assets/images/header/menu.svg" alt="menu" />
             </button>
-            <a href="#" class="logo">UKRAIDA<img class="nav__img" src="wp-content/themes/ukraida/src/images/header/logo.svg" alt="Logo" /></a>
+            <a href="#" class="logo">UKRAIDA<img class="nav__img" src="<?php bloginfo('template_url'); ?>/assets/images/header/logo.svg" alt="Logo" /></a>
             <nav class="nav">
                 <ul class="nav__site">
                     <li class="nav__item"><a class="link" href="#">Головна</a></li>
@@ -28,10 +28,10 @@
             </nav>
             <ul class="menu">
                 <li>
-                    <a href="#" class="menu__item-inst"><img src="wp-content/themes/ukraida/src/images/header/instagram.svg" alt="instagram" width="32" /></a>
+                    <a href="#" class="menu__item-inst"><img src="<?php bloginfo('template_url'); ?>/assets/images/header/instagram.svg" alt="instagram" width="32" /></a>
                 </li>
                 <li>
-                    <a href="#" class="menu__item"><img class="menu__img" src="wp-content/themes/ukraida/src/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE</span>/UA</a>
+                    <a href="#" class="menu__item"><img class="menu__img" src="<?php bloginfo('template_url'); ?>/assets/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE</span>/UA</a>
                 </li>
                 <li><button class="menu__btn">Задонатити</button></li>
             </ul>
@@ -42,11 +42,11 @@
                 <div class="js-menu-container">
                     <ul class="menu__modal">
                         <li>
-                            <a href="#" class="menu__modal-item"><img class="menu__modalimg" src="wp-content/themes/ukraida/src/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE&nbsp;</span>/&nbsp;UA</a>
+                            <a href="#" class="menu__modal-item"><img class="menu__modalimg" src="<?php bloginfo('template_url'); ?>/assets/images/header/language.svg" alt="language" /><span class="menu__itm-lang">DE&nbsp;</span>/&nbsp;UA</a>
                         </li>
                         <li>
                             <button class="modal js-close-menu">
-                                <img src="wp-content/themes/ukraida/src/images/header/close.svg" alt="close" width="17" />
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/header/close.svg" alt="close" width="17" />
                             </button>
                         </li>
                     </ul>
@@ -71,7 +71,7 @@
                         </li>
                     </ul>
                     <div class="menu__item-modal">
-                        <a href="#"><img class="nav__img-inst" src="wp-content/themes/ukraida/src/images/header/instagram.svg" alt="instagram" width="36" /></a>
+                        <a href="#"><img class="nav__img-inst" src="<?php bloginfo('template_url'); ?>/assets/images/header/instagram.svg" alt="instagram" width="36" /></a>
                     </div>
                 </div>
             </div>
