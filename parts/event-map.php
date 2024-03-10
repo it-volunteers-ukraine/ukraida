39ddee0147c6b6b9074b64dbacd01614dc6b8c76
Smilichenko <110824059+SergSmilya@users.noreply.github.com>
2024-03-10 16:08:36 +0200

    Update event-map.php

--- parts/event-map.php
+++ parts/event-map.php
@@ -1,6 +1,6 @@
 <?php
     $currend_id = get_the_ID();
-    // $my_map_src = get_field('map_link', $currend_id);
+    $my_map_src = get_field('map_link', $currend_id);
 ?>
 <div class="event-map">
     <iframe src="<?php echo $my_map_src; ?>" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
