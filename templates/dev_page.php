62dbb2b8c4106c133ffd203da03ae8d81f773362
gladja <gladkih.alexander@gmail.com>
2024-03-12 01:01:34 +0200

    created dev_page, add function.php dev_page style

---
+++ templates/dev_page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<section>
+    <div class="dev__page">
+        <img class="dev__page-img1" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img1.png" alt="pigeon" />
+        <img class="dev__page-img2" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img2.png" alt="pigeon" />
+        <img class="dev__page-img3" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img3.png" alt="pigeon" />
+        <div class="text__wrap">
+            <h2 class="title__text">Сторінка знаходиться в розробці</h2>
+            <p class="text__item">
+                Вибачте за тимчасові незручності! Ми працюємо над створенням
+                чогось особливого. Дякуємо за терпіння!
+            </p>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
