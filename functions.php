d3d6fd300dc09121b21023e0dce3d1ca5b22b4fe
NilKad <kadulin@gmail.com>
2024-03-12 11:23:01 +0100

    dev_page

--- functions.php
+++ functions.php
@@ -79,9 +79,7 @@ function wp_it_volunteers_scripts()
     wp_enqueue_style( 'error-style', get_stylesheet_directory_uri() . '/assets/styles/template-styles/404.css' , array('main'));
   }
 
-  if ( is_page_template('templates/dev_page.php') ){
-    wp_enqueue_style('dev_page-style', get_template_directory_uri() . '/assets/styles/template-styles/dev_page.css', array());
-  }
+  
 
   // if (is_singular() && is_page_template('parts/gallery.php')) {
   //   wp_enqueue_style( 'gallery-parts-style', get_template_directory_uri() . '/assets/styles/parts-styles/gallery.css', array() );
