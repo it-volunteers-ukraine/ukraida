94487affd601d28eea742e684216d1a93e9ecfe8
NilKad <kadulin@gmail.com>
2024-03-12 16:47:35 +0100

    add link dev_page

--- templates/dev_page.php
+++ templates/dev_page.php
@@ -1,9 +1,13 @@
-<?php get_header(); ?>
-<section>
+<?php get_header(); 
+/*
+Template Name: devpage
+*/
+?>
+<section class="">
     <div class="dev__page">
-        <img class="dev__page-img1" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img1.png" alt="pigeon" />
-        <img class="dev__page-img2" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img2.png" alt="pigeon" />
-        <img class="dev__page-img3" src="wp-content/themes/ukraida/src/images/dev_page/dev_page-img3.png" alt="pigeon" />
+        <img class="dev__page-img1" src="<?php bloginfo('template_url'); ?>/assets/images/dev_page/dev_page-img1.png" alt="pigeon" />
+        <img class="dev__page-img2" src="<?php bloginfo('template_url'); ?>/assets/images/dev_page/dev_page-img2.png" alt="pigeon" />
+        <img class="dev__page-img3" src="<?php bloginfo('template_url'); ?>/assets/images/dev_page/dev_page-img3.png" alt="pigeon" />
         <div class="text__wrap">
             <h2 class="title__text">Сторінка знаходиться в розробці</h2>
             <p class="text__item">
