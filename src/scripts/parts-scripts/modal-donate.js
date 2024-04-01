f17cc8542dfd8caa60f9cfb87e06fa2c889d1ae0
NilKad <kadulin@gmail.com>
2024-04-01 17:02:45 +0200

    change copy

--- src/scripts/parts-scripts/modal-donate.js
+++ src/scripts/parts-scripts/modal-donate.js
@@ -1,65 +1,72 @@
-const btnDonateRef = document.getElementById('js-btn-donate');
-const btnCloseModalRef = document.getElementById('js-btn-close');
-const modalContainerRef = document.getElementById('js-modal');
-const modalInnerRef = document.getElementById('js-inner__wrap');
-const btnDonateCopyRef = document.getElementById('js-btn-donate-copy');
+const btnDonateRef = document.getElementById("js-btn-donate");
+const btnCloseModalRef = document.getElementById("js-btn-close");
+const modalContainerRef = document.getElementById("js-modal");
+const modalInnerRef = document.getElementById("js-inner__wrap");
+const btnDonateCopyRef = document.getElementById("js-btn-donate-copy");
 
-console.log('modal script')
+console.log("modal script");
 
 function openModal() {
-    modalContainerRef.classList.add('is-open');
-    modalInnerRef.classList.add('is-open');
-    modalContainerRef.addEventListener('click', closeModal); 
-    document.addEventListener('keydown', closeModal);
-    document.body.classList.add('modal-open');
-    // window.scroll(0, 0);
-    
-    btnDonateCopyRef.addEventListener('click', copyIban);
+  modalContainerRef.classList.add("is-open");
+  modalInnerRef.classList.add("is-open");
+  modalContainerRef.addEventListener("click", closeModal);
+  document.addEventListener("keydown", closeModal);
+  document.body.classList.add("modal-open");
+  // window.scroll(0, 0);
+
+  btnDonateCopyRef.addEventListener("click", copyIban);
 }
 
 function closeModal(evt) {
-    if (evt.code === 'Escape' || evt.currentTarget === evt.target || evt.currentTarget.id === 'js-btn-close') {
-        modalInnerRef.classList.remove('is-open');
-        modalContainerRef.classList.remove('is-open');
-        document.removeEventListener('keydown', closeModal);
-        modalContainerRef.removeEventListener('click', closeModal); 
+  if (
+    evt.code === "Escape" ||
+    evt.currentTarget === evt.target ||
+    evt.currentTarget.id === "js-btn-close"
+  ) {
+    modalInnerRef.classList.remove("is-open");
+    modalContainerRef.classList.remove("is-open");
+    document.removeEventListener("keydown", closeModal);
+    modalContainerRef.removeEventListener("click", closeModal);
 
-        btnDonateCopyRef.removeEventListener('click', copyIban);
-        document.body.classList.remove('modal-open');
-    }
+    btnDonateCopyRef.removeEventListener("click", copyIban);
+    document.body.classList.remove("modal-open");
+  }
 }
 
 function copyIban() {
-    const ibanFieldRef = document.getElementById('js-iban').innerText;
-    const el = document.createElement('textarea');
-    el.value = ibanFieldRef;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+  const ibanValue = document.getElementById("js-iban").innerText;
+  try {
+    navigator.clipboard.writeText(ibanValue);
+  } catch (error) {
+    if (!window.isSecureContext) {
+      console.log("Cant copy non securyti https");
+    } else {
+      console.log("Error", error);
+    }
+  }
 
-    // Notification
-    btnDonateCopyRef.style.position = 'relative';
+  // Notification
+  btnDonateCopyRef.style.position = "relative";
 
-    const notify = document.createElement('div');
-    notify.style.backgroundImage = 'url(wp-content/themes/ukraida/src/images/icons/success.svg)';
-    notify.style.width = '24px';
-    notify.style.height = '24px';
-    notify.style.borderRadius = '50%';
-    notify.style.position = 'absolute';
-    notify.style.top = '-50%';
-    notify.style.right = '-15%';
+  const notify = document.createElement("div");
+  
+  if (btnDonateCopyRef.children.length == 0) {
+    notify.style.backgroundImage =
+      "url(wp-content/themes/ukraida/src/images/icons/success.svg)";
+    notify.style.width = "24px";
+    notify.style.height = "24px";
+    notify.style.borderRadius = "50%";
+    notify.style.position = "absolute";
+    notify.style.top = "-12px";
+    notify.style.right = "-24px";
 
     btnDonateCopyRef.appendChild(notify);
-    
     setTimeout(() => {
-        btnDonateCopyRef.removeChild(notify);
-        btnDonateCopyRef.style.position = 'static';
-     }, 2000);
+      btnDonateCopyRef.removeChild(notify);
+      btnDonateCopyRef.style.position = "static";
+    }, 2000);
+  }
 }
 
-btnDonateRef.addEventListener('click', openModal);
-btnCloseModalRef.addEventListener('click', closeModal);
+btnDonateRef.addEventListener("click", openModal);
+btnCloseModalRef.addEventListener("click", closeModal);
