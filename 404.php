8d56536851a904deb0eae21538991b8a9a2b3445
Tetiana Fedorenko <1031213@gmail.com>
2024-04-05 14:46:41 +0300

    fix: 404 add new text

--- 404.php
+++ 404.php
@@ -7,12 +7,17 @@ get_header();
 <main>
     <section class="error section">
         <div class="container">
-        <h1 class="error__title">404</h1>
-        <p>Cторінка видалина або не активна</p>
-        <a href="/" class="button-secondary">Повернутись на головну</a>
+            <h1 class="error__title">404</h1>
+            <p>Diese Seite wurde nicht gefunden.</p>
+            <p>Leider gibt's die Seite, die Sie gesucht
+                haben, auf der UKRAIDA Webseite
+                nich</p>
+            <p>Schreiben Sie uns bitte, vielleicht
+                können wir Ihnen helfen.</p>
+            <a href="/" class="button-secondary">Zur Startseite</a>
         </div>
-        
-        
+
+
     </section>
 
 
