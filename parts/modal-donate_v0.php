1400f1e236c63284feb5ee8d47df4bba6dd40bd5
NilKad <kadulin@gmail.com>
2024-04-05 14:30:38 +0200

    fix footer

--- parts/modal-donate_v0.php
+++ parts/modal-donate_v0.php
@@ -1,15 +1,16 @@
 <!-- event-map розкоментувати 3 рядок -->
 
-<!-- <div id='js-modal' class="modal">
+<div id='js-modal' class="modal">
     <div class="container">
         <div id="js-inner__wrap" class="inner__wrap">
-            
+            <!-- button-close -->
             <button id="js-btn-close" type="button" class="btn-clear btn-icon modal__btn-close">
                 <svg class="modal__icon-btn">
                     <use href="<?php bloginfo('template_url'); ?>/assets/images/footer/sprites.svg#close"></use>
                 </svg>
             </button>
 
+            <!-- text -->
             <div class="modal__text-wrap">
                 <h2 class="modal__title">Міжнародний благодійний фонд "UKRAIDA"</h2>
                 <ul class="payment-list">
@@ -26,29 +27,11 @@
                 <h3 class="modal__subtitle">Призначення платежу:</h3>
                 <p class="modal__subtitle-text">“Безповоротна фінансова допомога від ПІБ”</p>
             </div>
+            <!-- button-copy -->
             <button id="js-btn-donate-copy" type="button" class="modal__btn-copy">Копіювати IBAN</button>
         </div>
     </div>
-</div> -->
-<div id="donat-modal" class="modal">
-    <div class="modal-wrap">
-        <h2 class="donat-title"></h2>
-        <div class="item donat-item">
-            <p class="item-title">Name des Empfängers:</p>
-            <p class="item-text">Ukrainischer Verein Darmstadt (UKRAIDA) e.V.</p>
-        </div>
-        <div class="item donat-item">
-            <p class="item-title">IBAN:</p>
-            <p class="item-text">DE25 5085 0150 0080 0148 99</p>
-        </div>
-        <div class="item donat-item">
-            <p class="item-title">Verwendungszweck:</p>
-            <p class="item-text">Spende</p>
-        </div>
-    </div>
 </div>
-
 <Script>
-    const themeUrl = "<?php echo esc_url(get_template_directory_uri()); ?>"
-    const backgroundImageUrl = "<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/icons/success.svg";
+  const backgroundImageUrl = "<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icons/success.svg";
 </Script>
---
+++ src/scripts/parts-scripts/modal-donate_v0.js
@@ -0,0 +1,95 @@
+const btnDonateRef = document.getElementById("js-btn-donate");
+const btnCloseModalRef = document.getElementById("js-btn-close");
+const modalContainerRef = document.getElementById("js-modal");
+const modalInnerRef = document.getElementById("js-inner__wrap");
+const btnDonateCopyRef = document.getElementById("js-btn-donate-copy");
+
+console.log("modal script");
+
+function openModal() {
+  bodyScrollTop = window.scrollY || document.documentElement.scrollTop;
+    // Запрещаем прокрутку страницы
+  document.body.style.overflow = 'hidden';
+  document.body.style.paddingRight = getScrollbarWidth() + 'px';
+
+  modalContainerRef.classList.add("is-open");
+  modalInnerRef.classList.add("is-open");
+  modalContainerRef.addEventListener("click", closeModal);
+  document.addEventListener("keydown", closeModal);
+  // document.body.classList.add("modal-open");
+  // window.scroll(0, 0);
+
+  btnDonateCopyRef.addEventListener("click", copyIban);
+}
+
+function closeModal(evt) {
+  if (
+    evt.code === "Escape" ||
+    evt.currentTarget === evt.target ||
+    evt.currentTarget.id === "js-btn-close"
+  ) {
+    document.body.style.overflow = '';
+    // Удаляем отступ справа
+    document.body.style.paddingRight = '';
+    // Возвращаем скролл на прежнюю позицию
+    window.scrollTo(0, bodyScrollTop);
+
+    modalInnerRef.classList.remove("is-open");
+    modalContainerRef.classList.remove("is-open");
+    document.removeEventListener("keydown", closeModal);
+    modalContainerRef.removeEventListener("click", closeModal);
+
+    btnDonateCopyRef.removeEventListener("click", copyIban);
+    // document.body.classList.remove("modal-open");
+  }
+}
+
+function getScrollbarWidth() {
+  // Создаем элемент-индикатор прокрутки
+  var scrollDiv = document.createElement('div');
+  scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
+  document.body.appendChild(scrollDiv);
+  // Вычисляем ширину скроллбара
+  var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+  // Удаляем элемент-индикатор прокрутки
+  document.body.removeChild(scrollDiv);
+  return scrollbarWidth;
+}
+
+function copyIban() {
+  const ibanValue = document.getElementById("js-iban").innerText;
+  try {
+    navigator.clipboard.writeText(ibanValue);
+  } catch (error) {
+    if (!window.isSecureContext) {
+      console.log("Cant copy non securyti https");
+    } else {
+      console.log("Error", error);
+    }
+  }
+
+  // Notification
+  btnDonateCopyRef.style.position = "relative";
+
+  const notify = document.createElement("div");
+  // const backgroundImageUrl = "<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icons/success.svg";
+  const svgUrl = "url(" + backgroundImageUrl + ")";
+  if (btnDonateCopyRef.children.length == 0) {
+    notify.style.backgroundImage = svgUrl;
+    notify.style.width = "24px";
+    notify.style.height = "24px";
+    notify.style.borderRadius = "50%";
+    notify.style.position = "absolute";
+    notify.style.top = "-12px";
+    notify.style.right = "-24px";
+
+    btnDonateCopyRef.appendChild(notify);
+    setTimeout(() => {
+      btnDonateCopyRef.removeChild(notify);
+      btnDonateCopyRef.style.position = "static";
+    }, 2000);
+  }
+}
+
+btnDonateRef.addEventListener("click", openModal);
+btnCloseModalRef.addEventListener("click", closeModal);
