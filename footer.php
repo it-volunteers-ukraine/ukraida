3bf1632c8e2aa612800203dc1df3a36042195854
NilKad <kadulin@gmail.com>
2024-04-07 18:29:10 +0200

    footer acf

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
                     <a href="/" class="logo1"><img class="logo_img" src="<?php bloginfo('template_url'); ?>/assets/images/logo.svg" alt="Logo" /></a>
                     <ul class="privacy__list">
                         <li class="privacy__item">
-                            <a href="#" target="_blank" rel="noopener noreferrer" class="privacy__link">Impressum</a>
+                            <a href="<?php the_field('privacy_policy', 'option'); ?>" target="_blank" rel="noopener noreferrer" class="privacy__link">Impressum</a>
                         </li>
                         <li class="privacy__item privacy__item-order">
-                            <a href="#" target="_blank" rel="noopener noreferrer" class="privacy__link">Datenschutz</a>
+                            <a href="<?php the_field('privacy_data', 'option'); ?>" target="_blank" rel="noopener noreferrer" class="privacy__link">Datenschutz</a>
                         </li>
                     </ul>
                     <ul class="social-media__list">
