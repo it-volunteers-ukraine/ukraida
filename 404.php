21eca004ca485c6d5c169b13e3deaae32e5993cd
Tetiana Fedorenko <1031213@gmail.com>
2024-04-09 12:16:20 +0300

    fix:404 add new styles

--- 404.php
+++ 404.php
@@ -5,15 +5,17 @@ Template Name: 404
 get_header();
 ?>
 <main>
-    <section class="error section">
+    <section class="error">
         <div class="container">
             <h1 class="error__title">404</h1>
-            <p>Diese Seite wurde nicht gefunden.</p>
-            <p>Leider gibt's die Seite, die Sie gesucht
-                haben, auf der UKRAIDA Webseite
-                nich</p>
-            <p>Schreiben Sie uns bitte, vielleicht
-                können wir Ihnen helfen.</p>
+            
+            <p>Diese Seite wurde nicht gefunden. Leider gibt's die Seite,
+                 die Sie gesucht haben, auf der UKRAIDA Webseite nicht.</p>
+            <p>Schreiben Sie uns bitte, vielleicht können wir Ihnen helfen.</p>
+    <p>
+    <a class="error__mail" href="mailto:ukraidadarmstadt@gmail.com">ukraidadarmstadt@gmail.com</a>
+    </p>
+              
             <a href="/" class="button-secondary">Zur Startseite</a>
         </div>
 
