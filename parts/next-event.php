a1bcf94b7a120c4b21099d89a9e1b009490b30eb
NilKad <kadulin@gmail.com>
2024-04-22 16:54:29 +0200

    event&map to wp

--- parts/next-event.php
+++ parts/next-event.php
@@ -1,8 +1,13 @@
+<?php
+$currend_id = get_the_ID();
+// $evnt_title = get_field('event_map_link', $currend_id);
+// $event_button_map = get_field('event_button_map)', $currend_id);
+?>
 
 <div class="event next-event__event">
     <!-- <br>font Lato: <br> -->
     <p class="event-title">
-    Die nächste Demonstration für Frieden in der Ukraine findet am 13.04.2024 auf dem Friedensplatz (Darmstadt) um 10:30 Uhr statt.
+    <?php echo get_field('event_title', $currend_id); ?>
     </p>
     <!-- <p class="event-title">
         Die nächste Demonstration für den Frieden in der Ukraine findet am 3. Februar 2024 um 10:30 Uhr auf dem Friedensplatz (Darmstadt) statt.
@@ -15,5 +20,5 @@
         Die nächste Demonstration für den Frieden in der Ukraine findet am 3. Februar 2024 um 10:30 Uhr auf dem Friedensplatz (Darmstadt) statt.
     </p>
      <button class="event-btn"> Показати на карті</button> -->
-    <a href="#event-map" class="event-btn">Auf der Karte anzeigen</a>
+    <a href="#event-map" class="event-btn"><?php echo get_field('event_button_text', $currend_id); ?></a>
 </div>
