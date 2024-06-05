8064b340fad7a44b7fece67b46dcfdd65631c0bb
NilKad <kadulin@gmail.com>
2024-06-05 14:18:39 +0200

    fix global title

--- parts/donates1.php
+++ parts/donates1.php
@@ -7,7 +7,7 @@ $currend_id = get_the_ID();
         <?php get_template_part('parts/donates_money', null, []); ?>
     </div>
     <div class="textblock">
-        <h2 class="textblock-title"><?php echo get_field('global_title', $currend_id); ?></h2>
+        <h2 class="textblock-title"><?php echo get_field('donates1_title', $currend_id); ?></h2>
         <p class="textblock-text"><?php echo get_field('donates1_text', $currend_id); ?></p>
         <a href="/devpage" class="textblock-btn"><?php echo get_field('donates1_btn_name', $currend_id); ?></a>
     </div>
