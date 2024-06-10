a0366da2117c197904d9ec6d0f0558635605b895
gladja <gladkih.alexander@gmail.com>
2024-06-11 00:18:24 +0300

    add block money2

--- parts/donates_money2.php
+++ parts/donates_money2.php
@@ -2,3 +2,24 @@
 $currend_id = get_the_ID();
 ?>
 
+<section>
+    <div class="post-type">
+        <div class="post-type__wrap">
+            <img src="<?php echo  get_template_directory_uri() . '/assets/images/donates/img_dron.jpeg'; ?>" alt="foto">
+        </div>
+        <div class="post-type__block">
+            <p class="post-type__start">
+                Розпочато: 00.00.0000
+            </p>
+            <h3 class="post-type__title">великий збір на дрон для зсу</h3>
+            <p class="post-type__text">
+                Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+            <p class="post-type__value">Потрібно зібрати:</p>
+            <p class="post-type__sum">0000000</p>
+            <div class="post-type__button">
+                <button class="btn">Підтримати збір</button>
+            </div>
+        </div>
+    </div>
+</section>
