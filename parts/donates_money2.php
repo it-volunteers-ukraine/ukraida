894787a1186e63033fbf4386228f0634c8ed89dd
gladja <gladkih.alexander@gmail.com>
2024-06-12 23:13:09 +0300

    add 2 post and styled

--- parts/donates_money2.php
+++ parts/donates_money2.php
@@ -2,7 +2,7 @@
 $currend_id = get_the_ID();
 ?>
 
-<section>
+<section class="post-section">
     <div class="post-type">
         <div class="post-type__wrap">
             <img src="<?php echo  get_template_directory_uri() . '/assets/images/donates/img_dron.jpeg'; ?>" alt="foto">
@@ -18,7 +18,26 @@ $currend_id = get_the_ID();
             <p class="post-type__value">Потрібно зібрати:</p>
             <p class="post-type__sum">0000000</p>
             <div class="post-type__button">
-                <button class="btn">Підтримати збір</button>
+                <button class="img-text__btn">Підтримати збір</button>
+            </div>
+        </div>
+    </div>
+    <div class="post-type">
+        <div class="post-type__wrap">
+            <img src="<?php echo  get_template_directory_uri() . '/assets/images/donates/img_dron.jpeg'; ?>" alt="foto">
+        </div>
+        <div class="post-type__block">
+            <p class="post-type__start">
+                Розпочато: 00.00.0000
+            </p>
+            <h3 class="post-type__title">великий збір на дрон для зсу</h3>
+            <p class="post-type__text">
+                Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+            <p class="post-type__value">Потрібно зібрати:</p>
+            <p class="post-type__sum">0000000</p>
+            <div class="post-type__button">
+                <button class="img-text__btn">Підтримати збір</button>
             </div>
         </div>
     </div>
