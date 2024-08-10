ff08a56172e358ae708d3e576f0d2d585a148339
NilKad <kadulin@gmail.com>
2024-08-10 17:31:49 +0200

    hidden bt

--- parts/donates2.php
+++ parts/donates2.php
@@ -10,7 +10,9 @@ $currend_id = get_the_ID();
         <div class="don2__text">
             <?php echo get_field('donates2_text', $currend_id); ?>
         </div>
-        <a href="<?php echo get_field('donates2_button_url', $currend_id); ?>" class="img-text__btn"><?php echo get_field('donates2_button_text', $currend_id); ?></a>
+        <?php if (get_field('donates2_button_text', $currend_id)) : ?>
+            <a href="<?php echo get_field('donates2_button_url', $currend_id); ?>" class="img-text__btn"><?php echo get_field('donates2_button_text', $currend_id); ?></a>
+        <?php endif; ?>
     </div>
     <div class="don2__swip">
         <!-- <img src="<?php echo  get_template_directory_uri() . '/assets/images/donates/img_don2.jpg'; ?>" alt=''> -->
