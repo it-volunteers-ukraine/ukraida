8111f416224d51192edbd12e39c18caf17863cbf
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-08-12 23:50:55 +0300

    more classes

--- templates/projects.php
+++ templates/projects.php
@@ -5,9 +5,9 @@ Template Name: projects
     $currend_id = get_the_ID();
 ?>
 
-<main>
+<main class="projects-main">
     <section class="section projects">
-        <div class="container">
+        <div class="container container__projects">
             <h1 class="projects-title"><?php echo get_field('title', $currend_id); ?></h1>
             <?php get_template_part('parts/projects-page-filter') ?>
             <?php get_template_part('parts/projects-page-items') ?>
