fff25b0d380d6ff14878bf52acd1510de2ecb191
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2024-08-20 23:35:56 +0300

    page about us

--- src/scripts/template-scripts/about_page.js
+++ src/scripts/template-scripts/about_page.js
@@ -1,19 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const elements = document.querySelectorAll('.swiper-pagination-bullet');
-  console.log(elements)
-
-  elements.forEach(element => {
-    element.style.setProperty('left', '0px', 'important');
-    console.log(element.style);
-  });
-});
-
 const swiper = new Swiper(".swiper", {
-  // Параметры
   direction: "horizontal",
   loop: true,
-  slidesPerView: 1, // По умолчанию 1 слайд
-  spaceBetween: 24, // Расстояние между слайдами
+  slidesPerView: 1, 
+  spaceBetween: 24, 
   speed: 500,
 
   navigation: {
@@ -27,10 +16,9 @@ const swiper = new Swiper(".swiper", {
   },
 
   breakpoints: {
-    // Когда ширина экрана >= 1200px
     1200: {
-      slidesPerView: 2, // Показывать 2 слайда
-      spaceBetween: 5, // Расстояние между слайдами
+      slidesPerView: 2, 
+      spaceBetween: 5, 
     },
   },
 });
