5218b2afc7c4dd4f943048b0071ca8fbef7d0226
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-08-21 11:43:07 +0300

    Moved item to separate file

---
+++ parts/our-team-members-item.php
@@ -0,0 +1,25 @@
+<?php
+    // Item for our team's member
+
+    //
+    $photo_url = get_field('our_team_member_image');
+    $name = esc_html(get_field('our_team_member_name'));
+    $short_info = esc_html(get_field('our_team_member_short_information'));
+    //
+    $instagram_name = esc_html(get_field('our_team_member_instagram_name'));
+    $instagram_link = "https://www.instagram.com/$instagram_name/";
+?>
+<div class="our-team-members-items-item">
+    <img class="our-team-members-items-item-image" src="<?= $photo_url ?>"/>
+    <div class="our-team-members-items-item-info">
+        <span class="our-team-members-items-item-info-name">
+            <?= $name ?>
+        </span>
+        <span class="our-team-members-items-item-info-short-info">
+            <?= $short_info ?>
+        </span>
+        <span class="our-team-members-items-item-info-short-info">
+            <a href="<?= $instagram_link ?>"><?= $instagram_name ?></a>
+        </span>
+    </div>
+</div>
--- templates/our_team_members.php
+++ templates/our_team_members.php
@@ -24,29 +24,7 @@ Template Name: our team members
                     while ($query->have_posts()) {
                         $query->the_post();
                         // Project's item
-
-                        $photo_url = get_field('our_team_member_image');
-                        $name = esc_html(get_field('our_team_member_name'));
-                        $short_info = esc_html(get_field('our_team_member_short_information'));
-
-                        $instagram_name = esc_html(get_field('our_team_member_instagram_name'));
-                        $instagram_link = "https://www.instagram.com/$instagram_name/";
-                    ?>
-                        <div class="our-team-members-items-item">
-                            <img class="our-team-members-items-item-image" src="<?= $photo_url ?>"/>
-                            <div class="our-team-members-items-item-info">
-                                <span class="our-team-members-items-item-info-name">
-                                    <?= $name ?>
-                                </span>
-                                <span class="our-team-members-items-item-info-short-info">
-                                    <?= $short_info ?>
-                                </span>
-                                <span class="our-team-members-items-item-info-short-info">
-                                    <a href="<?= $instagram_link ?>"><?= $instagram_name ?></a>
-                                </span>
-                            </div>
-                        </div>
-                    <?php
+                        get_template_part('parts/our-team-members-item');
                     }
 
                     // Resetting postdata
