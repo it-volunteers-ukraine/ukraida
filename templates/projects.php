660a33ec0202be01d89c0afb99a97216d59ccb07
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-09-06 14:49:39 +0300

    Fixed projects page to use title from ACF

--- templates/projects.php
+++ templates/projects.php
@@ -8,7 +8,7 @@ Template Name: projects
 <main>
     <section class="section projects">
         <div class="container container__projects">
-            <h1 class="projects-title"><?php echo get_field('title', $currend_id); ?></h1>
+            <h1 class="projects-title"><?php echo get_field('projects_title', $currend_id); ?></h1>
             <?php get_template_part('parts/projects-page-filter') ?>
             <?php get_template_part('parts/projects-page-items') ?>
         </div>
