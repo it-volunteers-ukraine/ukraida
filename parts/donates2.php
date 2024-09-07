b2b700b44239e46fe388dc0b1b4c29fda2c5b24b
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-09-07 12:28:56 +0300

    Translations for service zone elements and page elements.

--- parts/donates2.php
+++ parts/donates2.php
@@ -10,8 +10,14 @@ $currend_id = get_the_ID();
         <div class="don2__text">
             <?php echo get_field('donates2_text', $currend_id); ?>
         </div>
-        <?php if (get_field('donates2_button_text', $currend_id) AND get_field('donates2_button_url', $currend_id)) : ?>
-            <a href="<?php echo get_field('donates2_button_url', $currend_id); ?>" class="img-text__btn"><?php echo get_field('donates2_button_text', $currend_id); ?></a>
+        <?php if (get_field('donates2_button_text', $currend_id) AND get_field('donates2_button_url', $currend_id)) : 
+            // Get the current translation for the button's url
+            $btn_url = get_field('donates2_button_url', $currend_id);
+            $translated_btn_url = PllHelper::get_current_translation($btn_url);
+        ?>
+            <a href="<?php echo $translated_btn_url; ?>" class="img-text__btn">
+                <?php echo get_field('donates2_button_text', $currend_id); ?>
+            </a>
         <?php endif; ?>
     </div>
     <div class="don2__swip">
