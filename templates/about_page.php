4acb95453962c5274cb3b79cdc5e16986d17f6fc
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-09-11 18:18:36 +0300

    Added translation for "Details" text in about us in media block. Replaced urls to dev page with translated url.

--- templates/about_page.php
+++ templates/about_page.php
@@ -9,6 +9,9 @@ $currend_id = get_the_ID();
 
 // Get the current translation for the devpage
 $dev_page_url = PllHelper::get_current_translation('/devpage');
+
+//
+$detailed_text = get_field('about_detailed_text');
 ?>
 
 <main class="about about__wrapper">
@@ -80,7 +83,7 @@ $dev_page_url = PllHelper::get_current_translation('/devpage');
                                             <?php 
                                             $link = get_sub_field('about_in_media_link');
                                             if( $link ): ?>
-                                        <div class="slider__item-link"><a href="<?php echo esc_url($link); ?>">Детальніше</a></div> 
+                                        <div class="slider__item-link"><a href="<?php echo esc_url($link); ?>"><?= $detailed_text ?></a></div> 
                                         <?php endif; ?>
                                     </div>
                                 </div>
@@ -90,7 +93,7 @@ $dev_page_url = PllHelper::get_current_translation('/devpage');
                         </div>
                         <div class="swiper-pagination about__slider-pagination"></div>
                         <div class="about__button-wrapper">
-                            <a href="<?php echo site_url('/devpage/'); ?>" class="about__button about__slider-button" rel="noopener noreferrer">
+                            <a href="<?= $dev_page_url ?>" class="about__button about__slider-button" rel="noopener noreferrer">
                                 <?php echo get_field('text_button', $currend_id); ?>
                             </a>
                         </div> 
@@ -131,7 +134,7 @@ $dev_page_url = PllHelper::get_current_translation('/devpage');
             </ul>
 
             <div class="about__slider-button about__slider-button-bottom">
-                <a href="<?php echo site_url('/devpage/'); ?>" class="about__button" rel="noopener noreferrer">
+                <a href="<?= $dev_page_url ?>" class="about__button" rel="noopener noreferrer">
                     <?php echo get_field('text_button', $currend_id); ?>
                 </a>
             </div>
