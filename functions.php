04606cad0fbc441cffea7843ccb05445c044e8c6
NilKad <kadulin@gmail.com>
2024-09-19 14:07:04 +0200

    load main.js

--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function wp_it_volunteers_scripts()
   wp_enqueue_style('main', get_stylesheet_uri());
   wp_enqueue_style('wp-style', get_template_directory_uri() . '/assets/styles/main.css', array('main'));
   wp_enqueue_style('normalize', 'https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/2.0.0/modern-normalize.min.css');
-  // wp_enqueue_script('wp-scripts', get_template_directory_uri() . '/assets/scripts/main.js', array(), false, true);
+  wp_enqueue_script('wp-scripts', get_template_directory_uri() . '/assets/scripts/main.js', array(), false, true);
   wp_enqueue_style('modal-donate', get_template_directory_uri() . '/assets/styles/parts-styles/modal-donate.css', array());
   wp_enqueue_script('modal-donate-scripts', get_template_directory_uri() . '/assets/scripts/parts-scripts/modal-donate.js', array(), false, true);
 
