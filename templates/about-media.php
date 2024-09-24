e9583e01b2c583f12cf612f0fe6705aac1a77442
gladja <gladkih.alexander@gmail.com>
2024-09-24 20:56:24 +0300

    add pagination

--- templates/about-media.php
+++ templates/about-media.php
@@ -4,16 +4,21 @@ Template Name: about media
 */
 ?>
 
-
 <main>
     <section class="section container about__media">
         <h1 class="about__media-title"><?php echo esc_html(get_field('about_us_in_the_media_title')); ?> </h1>
         <ul class="about__media-list">
             <?php
+            // Получаем текущую страницу
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+            // Параметры для вывода постов (по 4 поста на странице)
             $args = array(
                 'post_type' => 'post-types-about-med',
-                'posts_per_page' => -1,
+                'posts_per_page' => 4, // Количество постов на странице
+                'paged' => $paged, // Текущая страница
             );
+
             $query = new WP_Query($args);
 
             if ($query->have_posts()) :
@@ -33,23 +38,36 @@ Template Name: about media
                                 <p class="about__media-date"><?php the_field('about_media_date'); ?></p>
                                 <p class="about__media-text"><?php the_field('about_media_text'); ?></p>
                                 <p class="about__media-link">
-
                                     <a href="#">Перейти на статтю</a>
                                 </p>
-
                             </div>
                         </div>
                     </li>
-            <?php
+                <?php
                 endwhile;
-
-            endif;
-
-            wp_reset_postdata();
-            ?>
+                ?>
         </ul>
 
+        <!-- Pagination -->
+        <div class="about__media-pagination">
+            <?php
+                get_template_part(
+                    'parts/projects-page-pagination',
+                    null,
+                    ["current_page" => $paged, "max_num_pages" => $query->max_num_pages]
+                );
+            ?>
+        </div>
 
+
+
+    <?php
+            else :
+                echo '<p>' . __('Нет публикаций') . '</p>';
+            endif;
+            // Сбрасываем данные поста
+            wp_reset_postdata();
+    ?>
     </section>
 </main>
 
