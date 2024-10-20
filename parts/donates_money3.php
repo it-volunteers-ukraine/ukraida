206284d0ba18835b643e98dd93fe440adf6293e3
gladja <gladkih.alexander@gmail.com>
2024-10-20 23:42:21 +0300

    change img size

--- parts/donates_money3.php
+++ parts/donates_money3.php
@@ -45,10 +45,14 @@ $current_id = get_the_ID();
                             <img src="<?php echo esc_url($img['url']); ?>" alt="foto">
                         </div>
                         <div class="result-card">
-                            <h2 class="result-card_title"><?php echo esc_html(get_field('donates_money_title')); ?></h2>
-                            <p class="result-card_text"><?php echo esc_html(get_field('donates_money_text')); ?></p>
-                            <p class="result-card_sum-title"><?php echo esc_html($title_archive_sum); ?></p>
-                            <p class="result-card_sum-value"><?php echo esc_html(get_field('donates_money_sum')); ?></p>
+                            <div>
+                                <h2 class="result-card_title"><?php echo esc_html(get_field('donates_money_title')); ?></h2>
+                                <p class="result-card_text"><?php echo esc_html(get_field('donates_money_text')); ?></p>
+                            </div>
+                            <div>
+                                <p class="result-card_sum-title"><?php echo esc_html($title_archive_sum); ?></p>
+                                <p class="result-card_sum-value"><?php echo esc_html(get_field('donates_money_sum')); ?></p>
+                            </div>
                         </div>
                     </div>
                 </div>
