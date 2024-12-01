d69fd38201091ba7b6a820b346a52ec51ca5e87e
NilKad <kadulin@gmail.com>
2024-12-01 20:56:03 +0100

    update block socseti

--- single-event.php
+++ single-event.php
@@ -113,19 +113,19 @@ $image2 = get_field('image2', $current_id);
     </section>
     <section class="section section__socseti">
         <div class="container">
-            <h2 class="socseti-title">Поділитись новиною:</h2>
+            <h2 class="socseti-title"><?php echo esc_html(get_field('block_socseti_title', 'option'))?></h2>
             <div class="socseti-list">
-                <a class="socseti-item" href="">
+                <a class="socseti-item" href="https://www.instagram.com/<?php the_field('instagram', 'option') ?>" target="_blank">
                     <svg class="socseti-icon">
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
                     </svg>
                 </a>
-                <a class="socseti-item" href="">
+                <a class="socseti-item" href="<?php the_field('facebook_link', 'option') ?>" target="_blank">
                     <svg class="socseti-icon">
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-facebook"></use>
                     </svg>
                 </a>
-                <a class="socseti-item" href="">
+                <a class="socseti-item" href="<?php the_field('xtwitter_link', 'option') ?>" target="_blank">
                     <svg class="socseti-icon">
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-twitter"></use>
                     </svg>
