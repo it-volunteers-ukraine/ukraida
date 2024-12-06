2d39eefad5060c15009cc6711a9afd463f1a42c9
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-12-06 18:28:00 +0200

    Part of event item

--- parts/news-item-event.php
+++ parts/news-item-event.php
@@ -1,4 +1,19 @@
 <?php
+    $currend_id = get_the_ID();
 
+    // Preparing variables
+    // Image
+    $image = get_field('image1', $currend_id);
+    $img_tag = ImageHelper::get_image_tag($image, 'news-item-event-image');
+    // Badge text
+    $badge_text = $args['texts']['type'];
+    // Categories
+    $categories_terms = get_field('categoty', $currend_id);
+    $categories = [];
+    if ($categories_terms) {
+        $categories = array_map(function ($term) {
+            return $term->name;
+        }, $categories_terms);
+    }
 ?>
     <div>event</div>
