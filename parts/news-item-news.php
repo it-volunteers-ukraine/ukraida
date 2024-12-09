1d0c10f192a842789e5fe29668194db7d5e23fcd
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-12-09 06:14:13 +0200

    News item stub

--- parts/news-item-news.php
+++ parts/news-item-news.php
@@ -15,6 +15,12 @@
             return $term->name;
         }, $categories_terms);
     }
+    //
+    $title = esc_html(get_field('one_news_title', $currend_id));
+    $date = esc_html(get_field('one_news_date', $currend_id));
+    $text = esc_html(get_field('one_news_text', $currend_id));
+    $link = get_the_permalink($currend_id);
+    $link_text = $args['texts']['link_title'];
 ?>
     <div class="news-item-news">
         <div class="news-item-news-image-wrap">
@@ -22,13 +28,38 @@
             <div class="news-item-news-image-mask"></div>
         </div>
         <div class="news-item-news-badge"><?= $badge_text ?></div>
-        <div class="news-item-news-categories">
-            <?php
-                foreach ($categories as $category):
-                    ?>
-                        <div class="news-item-news-category"><?= $category?></div>
-                    <?php
-                endforeach;
-            ?>
+        <?php
+            // Display categories if there are some
+            if (count($categories)):
+                ?>
+                    <div class="news-item-news-categories">
+                        <?php
+                            foreach ($categories as $category):
+                                ?>
+                                    <div class="news-item-news-category"><?= $category?></div>
+                                <?php
+                            endforeach;
+                        ?>
+                    </div>
+                <?php
+            endif;
+        ?>
+        <div class="news-item-news-card">
+            <div class="news-item-news-info">
+                <div class="news-item-news-header">
+                    <h2 class="news-item-news-title">
+                        <?= $title ?>
+                    </h2>
+                    <div class="news-item-news-date">
+                        <?= $date ?>
+                    </div>
+                </div>
+                <div class="news-item-news-excerpt">
+                    <?= $text ?>
+                </div>
+            </div>
+            <div class="news-item-news-link">
+                <a href="<?= $link ?>"><?= $link_text ?></a>
+            </div>
         </div>
     </div>
