feb8dd7e0da300c756afd0acab86b30caf915410
Andrii Katsubo <andrii.katsubo@gmail.com>
2024-12-18 10:31:41 +0200

    fix

--- templates/reports.php
+++ templates/reports.php
@@ -7,11 +7,11 @@ Template Name: reports
 <main>
     <?php
         $current_id = get_the_ID();
-        $report_title = get_field('title', $current_id);
+        $reports_title = get_field('title', $current_id);
     ?>
     <section class="section reports">
         <div class="container reports">
-            <h1 class="reports-title"><?php echo $report_title; ?></h1>
+            <h1 class="reports-title"><?php echo $reports_title; ?></h1>
 
             <?php if (have_rows('links')) : ?>
                 <ul class="reports-links">
