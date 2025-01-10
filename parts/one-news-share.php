06df318a30ff0e270569d3bf8ab87a3817f741a0
Andrii Katsubo <andrii.katsubo@gmail.com>
2025-01-10 15:49:16 +0200

    Share the post url using the share buttons (Facebook, X)

--- parts/one-news-share.php
+++ parts/one-news-share.php
@@ -1,3 +1,6 @@
+<?php
+  $this_page_url = get_the_permalink();
+?>
 <section class="section one-news__share">
   <h2 class="one-news-share__title">
     <?php 
@@ -10,21 +13,36 @@
   </h2>
   <ul class="one-news-share__group">
       <li class="one-news-share__social">
-        <a class="one-news-share__instagram one-news-share__icon" href="https://www.instagram.com/<?php the_field('instagram', 'option') ?>" target="_blank" rel="noopener noreferrer">
+        <a 
+          class="one-news-share__instagram one-news-share__icon"
+          href="https://www.instagram.com" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <svg class="one-news-share__svg">
             <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
           </svg>
         </a>
       </li>
       <li class="one-news-share__social">
-        <a class="one-news-share__facebook one-news-share__icon" href="<?php the_field('facebook_link', 'option') ?>" target="_blank" rel="noopener noreferrer">
+        <a 
+          class="one-news-share__facebook one-news-share__icon" 
+          href="https://www.facebook.com/sharer/sharer.php?u=<?= $this_page_url ?>"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <svg class="one-news-share__svg">
             <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-facebook"></use>
           </svg>
         </a>
       </li>
       <li class="one-news-share__social">
-        <a class="one-news-share__xtwitter one-news-share__icon" href="<?php the_field('xtwitter_link', 'option') ?>" target="_blank" rel="noopener noreferrer">
+        <a 
+          class="one-news-share__xtwitter one-news-share__icon" 
+          href="https://twitter.com/intent/tweet?text=<?= $this_page_url ?>" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           <svg class="one-news-share__svg">
             <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-twitter"></use>
           </svg>
